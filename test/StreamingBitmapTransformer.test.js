48133f7df2892f9a151a5d1ee6cb5df58aff1e7e
Arthur Jen <arthurljen@gmail.com>
2018-06-28 17:23:40 -0700

    transform test written

---
+++ test/StreamingBitmapTransformer.test.js
@@ -0,0 +1,27 @@
+const { join } = require('path');
+const StreamingBitmapTransformer = require('../lib/StreamingBitmapTransformer');
+const { unlink, readFile } = require('fs').promises;
+const { invert } = require('../lib/invert-transformer');
+const assert = require('assert');
+
+describe.only('streaming bitmap transformer', () => {
+    const source = join(__dirname, 'test-bitmap.bmp');
+    const invertedBitmap = './test/stream-invert.bmp';
+
+    beforeEach(() => {
+        return unlink(invertedBitmap)
+            .catch(err => {
+                if(err.code !== 'ENOENT') throw err;
+            });
+    });
+    
+    it('transforms an image', () => {
+        const streamingTransformer = new StreamingBitmapTransformer(source);
+        return streamingTransformer.transform(invert)
+            .then(() => {
+                const actual = readFile(invertedBitmap);
+                const expected = readFile('./test/inverted-expected.bmp');
+                assert.deepEqual(actual, expected);
+            });
+    });
+});
