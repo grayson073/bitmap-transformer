b741026ed3226bf379937944afb7bba341b00a21
Arthur Jen <arthurljen@gmail.com>
2018-07-06 15:56:42 -0700

    promises properly return in time for assertion

--- test/StreamingBitmapTransformer.test.js
+++ test/StreamingBitmapTransformer.test.js
@@ -1,30 +1,43 @@
 const { join } = require('path');
 const StreamingBitmapTransformer = require('../lib/StreamingBitmapTransformer');
-const { unlink, readFile } = require('fs').promises;
+const { unlink } = require('fs').promises;
+const fs = require('fs');
 const { invert } = require('../lib/invert-transformer');
 const assert = require('assert');
 
 describe.only('streaming bitmap transformer', () => {
     const source = join(__dirname, 'test-bitmap.bmp');
     const invertedBitmap = './test/stream-invert.bmp';
-
+    let streamer = null;
     beforeEach(() => {
         return unlink(invertedBitmap)
             .catch(err => {
                 if(err.code !== 'ENOENT') throw err;
             });
     });
+
+    beforeEach(() => {
+
+    });
     
     it('transforms an image', () => {
-        StreamingBitmapTransformer.create(source)
+        let actual = null;
+        let expected = null;
+        return StreamingBitmapTransformer.create(source)
             .then(streamingTransformer => {
-                return streamingTransformer.transform(invert, invertedBitmap);
-            })
-            .then(() => {
-                //TODO: Find out why actual = Promise{ <pending> }
-                const actual = readFile(invertedBitmap);
-                const expected = readFile('./test/inverted-expected.bmp');
-                assert.deepEqual(actual, expected);
-            });
+                return streamingTransformer.transform(invert, invertedBitmap)
+                    .then(() => {
+                        const actual = fs.readFileSync(invertedBitmap);
+                        const expected = fs.readFileSync('./test/inverted-expected.bmp');
+                        assert.deepEqual(actual, expected);
+                    })
+                    .catch(err => {
+                        throw err
+                    });
+        });
+        
+        
+        
+        
     });
 });
