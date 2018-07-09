5f9c845feb297bb3f72a4e778de8ac1f2622d4fc
Arthur Jen <arthurljen@gmail.com>
2018-07-09 10:09:26 -0700

    now accepts a single function in addition to array of functions

--- test/StreamingBitmapTransformer.test.js
+++ test/StreamingBitmapTransformer.test.js
@@ -3,6 +3,7 @@ const StreamingBitmapTransformer = require('../lib/StreamingBitmapTransformer');
 const { unlink } = require('fs').promises;
 const fs = require('fs');
 const { invert } = require('../lib/invert-transformer');
+const { blueify } = require('../lib/blueify-transformer')
 const assert = require('assert');
 
 describe('streaming bitmap transformer', () => {
@@ -15,18 +16,31 @@ describe('streaming bitmap transformer', () => {
                 if(err.code !== 'ENOENT') throw err;
             });
     });
-
-    beforeEach(() => {
-
-    });
     
-    it('transforms an image', () => {
+    
+
+    it('multiple transforms on an image', () => {
         return StreamingBitmapTransformer.create(source)
             .then(streamingTransformer => {
                 return streamingTransformer.transform([invert, invert, invert], invertedBitmap)
                     .then(() => {
                         const actual = fs.readFileSync(invertedBitmap);
                         const expected = fs.readFileSync('./test/inverted-expected.bmp');
+                        assert.deepEqual(actual, expected);
+                    })
+                    .catch(err => {
+                        throw err;
+                    });
+            });
+    });
+
+    it('transforms an image', () => {
+        return StreamingBitmapTransformer.create(source)
+            .then(streamingTransformer => {
+                return streamingTransformer.transform(blueify, invertedBitmap)
+                    .then(() => {
+                        const actual = fs.readFileSync(invertedBitmap);
+                        const expected = fs.readFileSync('./test/blueify-expected.bmp');
                         assert.deepEqual(actual, expected);
                     })
                     .catch(err => {
