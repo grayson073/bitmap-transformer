2e49851f1029f037f72d14b7b31df61e3bff8d13
Arthur Jen <arthurljen@gmail.com>
2018-07-09 10:13:53 -0700

    added missing semicolon

--- test/StreamingBitmapTransformer.test.js
+++ test/StreamingBitmapTransformer.test.js
@@ -3,7 +3,7 @@ const StreamingBitmapTransformer = require('../lib/StreamingBitmapTransformer');
 const { unlink } = require('fs').promises;
 const fs = require('fs');
 const { invert } = require('../lib/invert-transformer');
-const { blueify } = require('../lib/blueify-transformer')
+const { blueify } = require('../lib/blueify-transformer');
 const assert = require('assert');
 
 describe('streaming bitmap transformer', () => {
